2499f28ed17b8015fcb876fd62fb0861dea6d9bc
Gabija Nag <gabija.nag@gmail.com>
2019-09-12 14:56:08 +0300

    Put items into csv file

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ r = requests.get('https://www.bbc.com/news')
 soup = BeautifulSoup(r.content, 'lxml')
 
 # get all the article titles
-# headings = soup.find_all('h3')
 headings = soup.find_all(class_='gs-c-promo-heading__title')
 print(headings)
 headingslist =[]
@@ -23,3 +22,12 @@ for item in headings:
 print(len(headings)) #how many headings are there
 print(len(headingslist)) #check how many scraped
 print(headingslist)
+
+# put all the items into a csv file
+import numpy as np
+np.savetxt("Scraper.csv", headingslist, delimiter=",", fmt='%s', header='Article Titles')
+
+# in case you have multiple lists that need to be stacked
+#
+# np.savetxt("scraper.csv", np.column_stack((headingslist, \
+# urllist)), delimiter=",", fmt='%s', header=header)
